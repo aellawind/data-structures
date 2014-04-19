a34ed57b5da020d485bac68d7b9fd71bce76c33c
Hack Reactor Students <amiraanuar@gmail.com>
2014-04-18 21:55:06 -0700

    Completed hash table on sandbox branch

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,55 +11,36 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  // rehash function declaration
-  var rehash = function(hTable){
-    var temp = this._storage;
-    console.log("TEEEEMP", temp);
-    // console.log(temp);
-    for (var j = 0; j< hTable._limit/2; j++) {
-      if (hTable._storage.get(j)) {
-        var tuplesArray = hTable._storage.get(j);
-        temp.push(tuplesArray);
-      }
-    }
-
-    // remove everything from the hash table
-    // rehash using our temp array by readding each key value pair
-
-  };
-
-
-
-  // when we insert something, check to see if usage is 75%+
-  // if it is, then we want to double the limit
-  this._numValues++;
-  if (this._numValues/this._limit > 0.75) {
-    this._limit = this._limit * 2;
-    rehash(this);
-  }
-
-  // we want to set the value to an array of tuples
-  // the keys being the original keys
   if (this._storage.get(i)) {
     var val = this._storage.get(i);
     var tuple = [k,v];
     val.push(tuple);
+    this._numValues++;
   } else {
     var tuple = [k,v];
     this._storage.set(i, [tuple]);
+    this._numValues++;
   }
+
+  if (this._numValues/this._limit > 0.75) {
+    this.resize('up');
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var tupleArrays = this._storage.get(i);
-  console.log(tupleArrays);
-  for ( var i = 0; i < tupleArrays.length; i++ ){
-    if ( tupleArrays[i] === null ){
-      return null;
-    } else if ( tupleArrays[i][0] === k ){
-      return tupleArrays[i][1];
+  if ( tupleArrays ){
+    for ( var i = 0; i < tupleArrays.length; i++ ){
+      if ( tupleArrays[i] === null ){
+        return null;
+      } else if ( tupleArrays[i][0] === k ){
+        return tupleArrays[i][1];
+      }
     }
+  } else {
+    return null;
   }
 };
 
@@ -68,16 +49,54 @@ HashTable.prototype.remove = function(k){
   // listLoc is the hash for what we're looking for
   // i is the index of the list
   // v represents the array of tuples at that index
+  //
+
+  var that = this;
   this._storage.each(function(v, i, theList) {
     if ( listLoc === i ) {
       var tuplesArray = theList[i];
       for (var j = 0 ; j < tuplesArray.length; j++ ) {
         if (tuplesArray[j][0] === k) {
           tuplesArray[j] = null;
+          that._numValues--;
+        }
+      }
+    }
+  });
+
+  if (this._numValues/this._limit < 0.25) {
+    this.resize('down');
+  }
+};
+
+HashTable.prototype.resize = function(direction) {
+
+  var that = this;
+  var temp = this._storage;
+
+  if ( direction === 'up' ) {
+    this._limit = this._limit * 2;
+  } else {
+    this._limit = this._limit/2;
+  }
+
+  this._storage = makeLimitedArray(this._limit);
+  this._numValues = 0;
+
+  temp.each(function(value, index, storage){
+    if(value){
+      for(var i=0; i<value.length; i++){
+        if ( value[i] ){
+          that.insert(value[i][0], value[i][1]);
         }
       }
     }
   });
 
 };
+
+
+
+
+
 
